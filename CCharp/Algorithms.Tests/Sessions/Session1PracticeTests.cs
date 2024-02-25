c882aba23f3065a231daaffb4fd43a6746a03d54
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2024-02-25 02:38:05 +0200

    session one c# solutions for count factors, IsPrime, Rotate an array

---
+++ CCharp/Algorithms.Tests/Sessions/Session1PracticeTests.cs
@@ -0,0 +1,52 @@
+﻿using Algorithms.Sessions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Tests.Sessions
+{
+    public class Session1PracticeTests
+    {
+        [Theory]
+        [InlineData(5, 2)]
+        [InlineData(25, 3)]
+        [InlineData(6, 4)]
+        public void CountFactors_Should_Return_TheTotalNumbersOfDivisors_ForANaturalNumber(int input, int expected)
+        {
+            var sut = new Session1Practice();
+            var current = sut.CountFactors(input);
+            Assert.Equal(expected, current);
+        }
+
+        [Fact]
+        public void Rotate_ShouldRotateArray_WithKPosAtRight_ForKLowerThanN()
+        {
+            var sut = new Session1Practice();
+
+            var inputArray = new[] { 1, 2, 3, 4, 5, 6, 7 };
+            var positions = 3;
+
+            sut.Rotate(inputArray, positions);
+
+            Assert.Equal(5, inputArray[0]);
+            Assert.Equal(6, inputArray[1]);
+            Assert.Equal(7, inputArray[2]);
+            Assert.Equal(1, inputArray[3]);
+        }
+
+        [Theory]
+        [InlineData(4, 0)]
+        [InlineData(2, 1)]
+        [InlineData(1, 0)]
+        [InlineData(32, 0)]
+        public void IsPrime_Should_Validate_ANaturalNumber(int input, int expected)
+        {
+            var sut = new Session1Practice();
+            var current = sut.IsPrime(input);
+            Assert.Equal(expected, current);
+        }
+    }
+}
---
+++ CCharp/Algorithms/Sessions/Session1Practice.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+namespace Algorithms.Sessions
+{
+    public class Session1Practice
+    {
+        /// <summary>
+        /// https://www.geeksforgeeks.org/problems/number-of-factors1435/1
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns></returns>
+        public int CountFactors(int N)
+        {
+            var count = 0;
+            for (var i = 1; i * i <= N; i++)
+            {
+                if (N % i == 0)
+                {
+                    if (i * i == N)
+                        count++;
+                    else
+                        count += 2;
+                }
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// Rotate Array
+        /// O(n) T; O(1) S
+        /// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="k"></param>
+        public void Rotate(int[] nums, int k)
+        {
+            var n = nums.Length;
+            while (k > 0)
+            {
+                var move = nums[n - 1];
+                for (var i = n - 1; i > 0; i--)
+                {
+                    nums[i] = nums[i - 1];
+                }
+                nums[0] = move;
+                k--;
+            }
+            
+        }
+
+        private int[] Reverse(int[] input, int startIndex, int endIndex)
+        {
+            var temp = input[startIndex];
+            input[startIndex] = input[endIndex];
+            input[endIndex] = temp;
+            startIndex++;
+            endIndex--;
+            return input;
+        }
+
+        /// <summary>
+        /// https://www.geeksforgeeks.org/problems/prime-number2314/1
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns></returns>
+        public int IsPrime(int N)
+        {
+            if (N == 1)
+                return 0;
+
+            for (var i = 2; i * i <= N; i++)
+            {
+                if (N % i == 0)
+                    return 0;
+            }
+
+            return 1;
+        }
+    }
+}
