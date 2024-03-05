ee7edc54425366ccf73ddc31104deba39e80b9fd
Harsh Mangat <harshmangat0007@gmail.com>
2024-03-05 14:10:23 +0530

    pfsum

--- DSA-sessions/prefixsum/calculate-sum.js
+++ DSA-sessions/prefixsum/calculate-sum.js
@@ -4,7 +4,6 @@ function calsum(arr, q){
     let sum = 0;
     let L = q[i][0];
     let R = q[i][1];
-    console.log(L,R)
 
     for(let j= L; j <= R; j++){
        sum =  sum + arr[j];
@@ -13,14 +12,36 @@ function calsum(arr, q){
   }
  return result
 }
- 
-console.log(calsum([1,2,3,4,5,6,7],[[2,3], [4,5]]))
+// console.log(calsum([1,2,3,4,5,6,7],[[2,3], [4,5]]))
 
+function calpsum(arr, q){
+  let result = []
+  let psum = [];
+  psum[0] = arr[0]
+  
+  for(let i = 1; i < arr.length; i++){
+    psum[i] = psum[i - 1] + arr[i] 
+  }
 
-// for(let i = 0; i <= 7; i++){
-//   let sum= 0;
-//   sum = sum + i
-// }
+  for(let i=0; i< q.length; i++){
+    let sum = 0;
+    let L = q[i][0];
+    let R = q[i][1];
+    console.log(L,R)
+    if( L === 0 ){
+      sum = psum[R]
+    }else{
+      sum = psum[R] - psum[L - 1]
+    }
+    // for(let j= L; j <= R; j++){
+    //    sum =  sum + arr[j];
+    // }
+    result.push(sum)
+  }
+  return result
+}
+
+console.log(calpsum([1,2,3,4,5,6,7],[[2,3], [4,5]]))
 
 
 
