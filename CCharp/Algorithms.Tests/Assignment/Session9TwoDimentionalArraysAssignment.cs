0dfbfb17991adf5a303c11f8d552f01ffcd626e5
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2024-12-18 17:30:06 +0200

    class 2 dimentional arrays

---
+++ CCharp/Algorithms.Tests/Assignment/Session9TwoDimentionalArraysAssignment.cs
@@ -0,0 +1,68 @@
+﻿using Algorithms.Assignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Tests.Assignment
+{
+    public class Session9TwoDimensionalArraysAssignmentTests
+    {
+        [Fact]
+       /* [InlineData(new int[9][]
+        {
+            new int[] {-37,-15,-15,-8,9,17,41,45,48},
+            new int[] {-17,2,5,26,26,32,42,45,50},
+            new int[] {-8,3,16,36,41,46,47,50,50},
+            new int[] {0,13,19,45,45,49,49,50,50},
+            new int[] {0,34,42,49,49,50,50,50,50},
+            new int[] {2,34,49,50,50,50,50,50,50}, 
+            new int[] {9,44,50,50,50,50,50,50,50},
+            new int[] { 19, 48, 50, 50, 50, 50, 50, 50, 50 },
+            new int[] { 20, 50, 50, 50, 50, 50, 50, 50, 50 }
+        }, 7, 0)]*/
+        public void CountAllChocolate_ShouldReturn_TheMinNumberOfSwapsToMoveElementsGreaterThanKFirst()
+        {
+            var sut = new Session9TwoDimensionalArraysAssignment();
+
+            var test = new int[9][]
+            {
+                new int[9] {-37, -15, -15, -8, 9, 17, 41, 45, 48},
+                new int[9] {-17, 2, 5, 26, 26, 32, 42, 45, 50},
+                new int[] {-8, 3, 16, 36, 41, 46, 47, 50, 50},
+                new int[] {0, 13, 19, 45, 45, 49, 49, 50, 50},
+                new int[] {0, 34, 42, 49, 49, 50, 50, 50, 50},
+                new int[] {2, 34, 49, 50, 50, 50, 50, 50, 50},
+                new int[] {9, 44, 50, 50, 50, 50, 50, 50, 50},
+                new int[] {19, 48, 50, 50, 50, 50, 50, 50, 50},
+                new int[] {20, 50, 50, 50, 50, 50, 50, 50, 50}
+            };
+            var actual = sut.FindValueInMatrix(test, -50);
+            Assert.Equal(0, actual);
+        }
+
+
+       [Fact]
+       public void MyString_test()
+       {
+            // 5 \n
+            // 1 2 3 4 5\n
+            // 6 7 8 9 10\n
+            // 11 12 13 14 15\n
+            var input = "5\n1 2 3 4 5\n6 7 8 9 10\n11 12 13 14 15\n16 17 18 19 20\n21 22 23 24 25";
+            var inputString = input.Split('\n');
+            var n = int.Parse(inputString[0]);
+            var matrix = new int[n][];
+            for (var i = 0; i < n; i++)
+            {
+                matrix[i] = inputString[i + 1].Split(' ').Select(int.Parse).ToArray();
+            }
+            var sut = new Session9TwoDimensionalArraysAssignment();
+            var actual = sut.FindValueInMatrix(matrix, 5);
+            Assert.Equal(1, actual);
+        }
+
+
+    }
+}
---
+++ CCharp/Algorithms/Assignment/Session9TwoDimensionalArraysAssignment.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Tests.Assignment
+{
+    public class Session9TwoDimensionalArraysAssignment
+    {
+        public long MaxSumOfSubArayLengthK(int[] input, int k)
+        {
+            var n = input.Length;
+            long sum = 0;
+            long max = 0;
+            var s = 0;
+            var e = k - 1;
+            for (var i = 0; i < k; i++)
+            {
+                sum += input[i];
+            }
+            max = sum;
+            s++;
+            e++;
+            while (e < n)
+            {
+                sum = sum - input[s - 1] + input[e];
+                if (max < sum)
+                {
+                    max = sum;
+                }
+                s++;
+                e++;
+            }
+
+            return max;
+        }
+
+        public int FindValueInMatrix(int[][] matrix, int value)
+        {
+            var rows = matrix.GetLength(0);
+            var cols = rows;
+            var i = 0;
+            var j = cols - 1;
+            while (i < rows && j > 0)
+            {
+                if (matrix[i][ j] == value)
+                {
+                    return 1;
+                }
+
+                if (matrix[i][ j] > value)
+                {
+                    j--;
+                }
+                if (matrix[i][ j] < value)
+                {
+                    i++;
+                }
+            }
+            return 0;
+        }
+    }
+}
